9225566025c06808f1b8350f38695b87b13b3c7d
dpotier <derek.potier@gmail.com>
2013-09-14 09:15:16 -0500

    Basic dragon hooks

--- Assets/scripts/DragonBehavior.cs
+++ Assets/scripts/DragonBehavior.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class DragonBehavior : MonoBehaviour {
-	public float speed = 13.0f;
-	public float normalSpeed = 13.0f;
-	public float minSpeed = 7.0f;
-	public float speedDecrement = 0.5f;
+	public float currentSpeed = 13.0f;	//current speed
+	public float normalSpeed = 13.0f;	//normal speed
+	public float minSpeed = 7.0f;		//absolute minimum speed
+	public float speedDecrement = 0.5f; //rate at which speed is decremented.
+	public float speedIncrement = 0.5f; //rate at which the speed increases after decrement
 	
 	public bool isHit = false;
 	
@@ -13,18 +14,24 @@ public class DragonBehavior : MonoBehaviour {
 	
 	void FixedUpdate (){
 		
-		moveDirection = new Vector2(speed, 0);
+		moveDirection = new Vector2(currentSpeed, 0);
 		
 		//Check if speed == normalSpeed, if not then slowly increase speed until speed = normal speed
-		
+		if (currentSpeed < normalSpeed)
+			currentSpeed = currentSpeed + speedIncrement;
+		else
+			currentSpeed = normalSpeed;
 		
 		
 		transform.Translate(moveDirection * Time.deltaTime);
 	}
 	
 	void reduceSpeed() {
-		//called when collision happens. 
-		speed = speed - speedDecrement;
+		//called when collision happens.
+		if( currentSpeed > minSpeed)
+			currentSpeed = currentSpeed - speedDecrement;
+		else
+			currentSpeed = minSpeed;
 	}
 	
 }
