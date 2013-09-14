4cd755f71d3a4a3a19fe52498ea6ab21d02783b5
poemdexter <poemdexter@gmail.com>
2013-09-13 21:14:54 -0500

    messing with movement.

---
+++ Assets/scripts/CameraFollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+	
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed = 10.0f;
+	public float jumpPower = 25.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		ConstantForce constantForce = new ConstantForce();
+		constantForce.force = new Vector3(speed,0,0);
+		
+		rigidbody.constantForce = constantForce;
+//		
+//		Vector3 move = Vector3.zero;
+//		
+//		if (Input.GetKeyDown(KeyCode.Space))
+//		{
+//			move += new Vector3(0, jumpPower, 0);
+//		}
+//
+//		move += new Vector3(speed, 0, 0);
+//		
+//		transform.Translate(move * Time.deltaTime);
+	}
+}
