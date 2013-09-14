7ff8a33834885446149443333c9469a4e9bc1d13
poemdexter <poemdexter@gmail.com>
2013-09-14 00:10:38 -0500

    merging

---
+++ Assets/scripts/ThrowableObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowableObject : MonoBehaviour {
+	
+	public float throwSpeed = 400;
+	
+	private bool thrown = false;
+	
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+	
+	// called by Player when this object is thrown
+	public void Throw()
+	{
+		if(!thrown)
+		{
+			thrown = true;
+			// throw yourself
+			rigidbody.AddForce(new Vector2(0,throwSpeed));
+		}
+	}
+	
+	public bool hasBeenThrown()
+	{
+		return thrown;
+	}
+}
