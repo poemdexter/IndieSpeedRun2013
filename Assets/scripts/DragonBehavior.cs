d82982295ca263ee79c153caefa093d2a26bae12
dpotier <derek.potier@gmail.com>
2013-09-13 23:55:42 -0500

    started on dragon work

---
+++ Assets/scripts/DragonBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonBehavior : MonoBehaviour {
+	public float speed = 13.0f;
+	public float normalSpeed = 13.0f;
+	public float minSpeed = 7.0f;
+	public float speedDecrement = 0.5f;
+	
+	public bool isHit = false;
+	
+	public Vector2 moveDirection = Vector2.zero;
+	
+	void FixedUpdate (){
+		
+		moveDirection = new Vector2(speed, 0);
+		
+		//Check if speed == normalSpeed, if not then slowly increase speed until speed = normal speed
+		
+		
+		
+		transform.Translate(moveDirection * Time.deltaTime);
+	}
+	
+	void reduceSpeed() {
+		//called when collision happens. 
+		speed = speed - speedDecrement;
+	}
+	
+}
