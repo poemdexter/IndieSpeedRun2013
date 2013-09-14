2bdfe891be9cfba48fa19f0154dab99094d1e1de
poemdexter <poemdexter@gmail.com>
2013-09-14 15:39:33 -0500

    finished up title screen with credits sorta.  could use polish.

--- Assets/scripts/TitlePageScript.cs
+++ Assets/scripts/TitlePageScript.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class TitlePageScript : MonoBehaviour {
 	
+	public GameObject credits;
+	public bool creditsVisible = false;
+	
 	// load the game
 	void StartGame()
 	{
 		Application.LoadLevel(1);
+	}
+	
+	// show team credits
+	void ShowCredits()
+	{
+		Renderer[] renderers = credits.GetComponentsInChildren<Renderer>();
+	    foreach (Renderer r in renderers)
+		{
+	        r.enabled = !r.enabled;
+	    }
 	}
 }
