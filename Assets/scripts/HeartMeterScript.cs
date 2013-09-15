431ec3d23dba82b401871b4f0f4200b99987a981
rlseaton <xezton@gmail.com>
2013-09-15 03:23:41 -0400

    added working heart meter

---
+++ Assets/scripts/HeartMeterScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeartMeterScript : MonoBehaviour {
+	
+	private int maxHearts = 3;
+	private int currentHearts = 3;
+	private tk2dSprite heart;
+	
+	// change the last full heart to empty
+	public void DepleteHearts()
+	{
+		if (currentHearts > 0)
+		{
+			currentHearts--;
+			
+			for(int i = maxHearts; i > (currentHearts); i--)
+			{
+				heart = transform.FindChild("Heart"+i).GetComponent<tk2dSprite>();
+				heart.SetSprite("HeartEmpty");
+			}
+		}
+		else
+		{
+			// reset scene or w/e
+		}
+	}
+	
+	public bool IsHeartMeterEmpty()
+	{
+		if(currentHearts > 0)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
