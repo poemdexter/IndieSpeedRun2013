46637a42c9f456373d393cf399f89089422fa3bd
poemdexter <poemdexter@gmail.com>
2013-09-15 03:18:45 -0500

    title screen fade to black, fade music to off as well.

--- Assets/scripts/TitlePageScript.cs
+++ Assets/scripts/TitlePageScript.cs
@@ -5,11 +5,37 @@ public class TitlePageScript : MonoBehaviour {
 	
 	public GameObject credits;
 	public bool creditsVisible = false;
+	public bool fading = false;
 	
-	// load the game
+	public Texture2D blackPixel;
+	public float fadeSpeed = 0.2f;
+	
+	public float alpha = 0.0f;
+	
 	void StartGame()
 	{
-		Application.LoadLevel(1);
+		// fade to black
+		fading = true;
+	}
+	
+	void OnGUI()
+	{
+		if (fading)
+		{
+			alpha += fadeSpeed * Time.deltaTime;
+			
+			// fade screen
+			GUI.color = new Color(0,0,0,alpha);
+			GUI.depth = 100;
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), blackPixel);
+			
+			// fade music
+			audio.volume = 1 - alpha;
+			
+			// load level if done transition
+			if (alpha >= 1) 
+				Application.LoadLevel(1);
+		}
 	}
 	
 	// show team credits
