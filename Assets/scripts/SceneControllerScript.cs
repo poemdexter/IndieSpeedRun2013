b667f5d453207b03cbb7f64702071ceb6fa4c453
poemdexter <poemdexter@gmail.com>
2013-09-15 03:37:33 -0500

    dragon doesn't animate until activated, game scene fades in from black and then starts music.  scenecontroller child of camera.

--- Assets/scripts/SceneControllerScript.cs
+++ Assets/scripts/SceneControllerScript.cs
@@ -3,17 +3,50 @@ using System.Collections;
 
 public class SceneControllerScript : MonoBehaviour {
 	
-	// automatically starts the scene
-	void Start()
+	public bool fading = true;
+	public Texture2D blackPixel;
+	public float fadeSpeed = -0.2f;
+	public float alpha = 1.0f;
+	
+	public AudioClip musicStart;
+	public AudioClip musicLoop;
+	
+	void OnGUI()
 	{
+		if (fading)
+		{
+			alpha += fadeSpeed * Time.deltaTime;
+			
+			// fade screen
+			GUI.color = new Color(0,0,0,alpha);
+			GUI.depth = 100;
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), blackPixel);
+			
+			// load level if done transition
+			if (alpha <= 0)
+			{
+				fading = false;
+				StartGame();
+			}	
+		}
+	}
+		
+	void StartGame()
+	{
+		// start music
+		AudioSource[] sources = GetComponents<AudioSource>();
+		sources[0].clip = musicStart;
+		sources[0].loop = false;
+		sources[0].Play();
+		
+		float clipLength = musicStart.length;
+		sources[1].clip = musicLoop;
+		sources[1].loop = true;
+		sources[1].PlayDelayed(clipLength);
+		
 		// activate the dragon and player
 		GameObject.Find ("Player").GetComponent<PlayerMovement>().Activate();
 		GameObject.Find ("Dragon").GetComponent<DragonBehavior>().Activate();
-	}
-	
-	void Update()
-	{
-	
 	}
 	
 	public void Restart()
