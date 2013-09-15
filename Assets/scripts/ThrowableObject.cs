26b3a707327a3025829efe71eaf2092b6fb51794
rlseaton <xezton@gmail.com>
2013-09-15 05:12:18 -0400

    added peasant screams

--- Assets/scripts/ThrowableObject.cs
+++ Assets/scripts/ThrowableObject.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 /*
  * Allows an object to be thrown by the player and takes care of changing
@@ -8,9 +9,12 @@ using System.Collections;
 
 public class ThrowableObject : MonoBehaviour {
 	
+	public AudioClip scream1, scream2, scream3, scream4, scream5, scream6, scream7, scream8, scream9;
 	public Vector2 throwVector = new Vector2(-600, 600);
 	public float throwRotation = 400;
 	public bool thrown = false;
+	
+	private List<AudioClip> screams = new List<AudioClip>();
 	private tk2dSprite pointer;
 	private Vector3 pointerVector = new Vector3(0,0.1f,0);
 	
@@ -19,6 +23,16 @@ public class ThrowableObject : MonoBehaviour {
 		// set pointer's renderlayer to the same as its parent
 		pointer = transform.FindChild("Pointer").GetComponent<tk2dSprite>();
 		pointer.RenderLayer = GetComponent<tk2dSprite>().RenderLayer;
+		
+		screams.Add(scream1);
+		screams.Add(scream2);
+		screams.Add(scream3);
+		screams.Add(scream4);
+		screams.Add(scream5);
+		screams.Add(scream6);
+		screams.Add(scream7);
+		screams.Add(scream8);
+		screams.Add(scream9);
 	}
 	
 	void Update()
@@ -50,6 +64,9 @@ public class ThrowableObject : MonoBehaviour {
 			rigidbody.AddTorque(new Vector3(0,0,throwRotation));
 			rigidbody.useGravity = true;									// necessary to make object throw more nicely
 			DestroyObject(pointer);
+			
+			// play peasant scream sound
+			AudioSource.PlayClipAtPoint(screams[Random.Range( 0, screams.Count )], transform.position);
 		}
 	}
 	
